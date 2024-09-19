8b11f3adc894312a621ea6e31ef2e9152ee3cd75
Leonardo Macieira <leonardomacieira10@gmail.com>
2024-09-19 13:45:35 -0300

    adicionando projetos

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -12,9 +12,9 @@ const ProjectCard = ({ project }) => {
         {/* image */}
         <div className="relative w-full h-[300px] flex items-center justify-center bg-tertiary dark:bg-secondary/40 xl:bg-work_project_bg_light xl:dark:bg-work_project_bg_dark xl:bg-[110%] xl:bg-no-repeat overflow-hidden">
           <Image
-            className="absolute bottom-0 shadow-2xl"
+            className="absolute shadow-2xl"
             src={project.image}
-            width={247}
+            width={250}
             height={250}
             alt=""
             priority
